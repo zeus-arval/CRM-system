f41c9afef867d0ec96265e05b7360e06702ba110
Artur Valdna <arturvaldna@gmail.com>
2022-03-06 22:13:47 +0200

    added templates for sidebar and navbar

---
+++ src/components/app/SideBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: isOpen}">  
+
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <li>
+        <a href="#" class="waves-effect waves-orane pointer">{{link.title}}</a>
+      </li>
+    </router-link>
+    
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['isOpen'],
+  data: () => ({
+    links: [
+      {title: 'Счет', url: '/', exact: true},
+      {title: 'История', url: '/history'},
+      {title: 'Планирование', url: '/planning'},
+      {title: 'Новая запись', url: '/record'},
+      {title: 'Категория', url: '/categories'},
+    ]
+  })
+}
+</script>
