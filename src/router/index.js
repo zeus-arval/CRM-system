cdf7da4a51e4c6043ce0b0ab4a1e51104ec066e5
Artur Valdna <arturvaldna@gmail.com>
2022-03-06 11:43:53 +0200

    Added vue pages

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,56 @@ const routes = [
   {
     path: '/',
     name: 'home',
+    meta: {layout: 'main'},
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/CategoriesView.vue')
+  },
+  {
+    path: '/detailrecord',
+    name: 'detailrecord',
+    meta: {layout: 'main'},
+    component: () => import('../views/DetailRecordView.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/HistoryView.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/PlanningView.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/ProfileView.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/RecordView.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/RegisterView.vue')
   }
 ]
 
